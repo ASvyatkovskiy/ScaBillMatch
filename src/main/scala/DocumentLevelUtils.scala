bff25c584348b1ee50ba76c821c5a72f6d915d1a
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2016-05-09 16:09:11 -0400

    Specify return type in similarity function

--- src/main/scala/DocumentLevelUtils.scala
+++ src/main/scala/DocumentLevelUtils.scala
@@ -30,11 +30,11 @@ object DocumentLevelUtils {
     wGrps.distinct
   }
 
-  def extractSimilarities = (grps: Tuple2[ArrayBuffer[Long], ArrayBuffer[Long]]) => {
+  def extractSimilarities (grps: Tuple2[ArrayBuffer[Long], ArrayBuffer[Long]]) : Double = {
     val s_wGrps = grps._1
     val m_wGrps = grps._2
     val model_size = m_wGrps.union(s_wGrps).length
-    val matchCnt = m_wGrps.intersect(s_wGrps).length
+    val matchCnt = m_wGrps.intersect(s_wGrps).length.toDouble
     matchCnt/model_size * 100.
   }
 
