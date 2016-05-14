d727bba4a6fd138a8b5f27193e8610cdc66ebd02
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2016-05-13 22:44:27 -0400

    opt instead of arg, configure all input and output files as params

--- src/main/scala/DocumentAnalyzer.scala
+++ src/main/scala/DocumentAnalyzer.scala
@@ -8,7 +8,7 @@ import org.apache.spark.sql.functions._
 
 object DocumentAnalyzer {
 
-  case class Params(inputFile: String = null, secThreshold: Double = 70.0)
+  case class Params(inputBillsFile: String = null, inputPairsFile: String = null, outputMainFile: String = null, outputFilteredFile: String = null, secThreshold: Double = 70.0)
     extends AbstractParams[Params]
 
   //get type of var utility 
@@ -26,10 +26,22 @@ object DocumentAnalyzer {
         .required()
         .text(s"Minimum Jaccard similarity to inspect on section level")
         .action((x, c) => c.copy(secThreshold = x))
-      arg[String]("<inputFile>")
+      opt[String]("inputBillsFile")
         .required()
-        .text(s"input file, one JSON per line, located in NFS by default (file://)")
-        .action((x, c) => c.copy(inputFile = x))
+        .text(s"Bill input file, one JSON per line")
+        .action((x, c) => c.copy(inputBillsFile = x))
+      opt[String]("inputPairsFile")
+        .required()
+        .text(s"CartesianPairs object input file")
+        .action((x, c) => c.copy(inputPairsFile = x))
+      opt[String]("outputMainFile")
+        .required()
+        .text(s"outputMainFile: key-key pairs and corresponding similarities, as Tuple2[Tuple2[Long,Long],Double]")
+        .action((x, c) => c.copy(outputMainFile = x))
+      opt[String]("outputFilteredFile")
+        .required()
+        .text(s"outputFilteredFile: CartesianPairs passing similarity threshold")
+        .action((x, c) => c.copy(outputFilteredFile = x))
       note(
         """
           |For example, the following command runs this app on a dataset:
@@ -37,7 +49,9 @@ object DocumentAnalyzer {
           | spark-submit  --class DocumentAnalyzer \
           | --master yarn-client --num-executors 40 --executor-cores 2 --executor-memory 15g \
           | target/scala-2.10/BillAnalysis-assembly-1.0.jar \
-          | --secThreshold 70.0 /scratch/network/alexeys/bills/lexs/bills_3.json
+          | --secThreshold 70.0 --inputBillsFile /scratch/network/alexeys/bills/lexs/bills_3.json \
+          | --inputPairsFile /user/alexeys/test_object --outputMainFile /user/alexeys/test_main_output \
+          | --outputFilteredFile /user/alexeys/test_new_filtered_pairs
         """.stripMargin)
     }
 
@@ -68,13 +82,13 @@ object DocumentAnalyzer {
     val sqlContext = new org.apache.spark.sql.SQLContext(spark)
     import sqlContext.implicits._
 
-    var bills = sqlContext.read.json("file://"+params.inputFile).as[Document]
+    var bills = sqlContext.read.json(params.inputBillsFile).as[Document]
     //for more than 30 states, bump over 2000 partitions
     bills = bills.repartition(2005) 
 
     //First, run the hashing step here
     val hashed_bills = bills.rdd.map(bill => (bill.primary_key,bill.content)).mapValues(content => DocumentLevelUtils.preprocess(content)).cache()
-    val cartesian_pairs = spark.objectFile[CartesianPair]("/user/alexeys/test_object").map(pp => (pp.pk1,pp.pk2))
+    val cartesian_pairs = spark.objectFile[CartesianPair](params.inputPairsFile).map(pp => (pp.pk1,pp.pk2))
 
     val firstjoin = cartesian_pairs.map({case (k1,k2) => (k1, (k1,k2))})
         .join(hashed_bills)
@@ -83,11 +97,11 @@ object DocumentAnalyzer {
         .join(hashed_bills)
         .map({case(_, (((k1,k2), v1), v2))=>((k1, k2),(v1, v2))}).mapValues(pp => DocumentLevelUtils.extractSimilarities(pp)) //.cache()
 
-    matches.saveAsObjectFile("/user/alexeys/test_main_output")
+    matches.saveAsObjectFile(params.outputMainFile)
 
     //scala.Tuple2[Long, Long]
     val threshold = params.secThreshold
-    matches.filter(kv => (kv._2 > threshold)).keys.saveAsObjectFile("/user/alexeys/test_new_filtered_pairs")
+    matches.filter(kv => (kv._2 > threshold)).keys.saveAsObjectFile(params.outputFilteredFile)
 
     spark.stop()
   }
