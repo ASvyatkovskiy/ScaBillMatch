0920174727c65c8695704a616197897db4baedc7
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2016-11-09 20:22:36 -0500

    Cleanup, refactor

--- src/main/scala/MakeLabeledCartesian.scala
+++ src/main/scala/MakeLabeledCartesian.scala
@@ -2,16 +2,16 @@
 Application: MakeLabeledCartesian, produce all the pairs of primary keys of the documents satisfying a predicate.
 Perform document bucketing using k-means clustering.
 
-Following parameters need to be filled in the resources/makeCartesian.conf file:
+Following are the key parameters that need to be filled in the resources/makeCartesian.conf file:
 	docVersion: document version: consider document pairs having a specific version. E.g. Introduced, Enacted...
-	nPartitions: number of partitions in bills_meta RDD
+        useLSA: whether to use truncated SVD
+        numConcepts: number of concepts to use for LSA
+        kval: number of clusters for k-means
         onlyInOut: a switch between in-out of state and using both in-out and in-in state pairs
 	use_strict: boolean, yes or no to consider strict parameters
-	strict_state: specify state (a long integer from 1 to 50) for one-against-all user selection (strict)
-	strict_docid: specify document ID for one-against-all user selection (strict)
-	strict_year: specify year for one-against-all user selection (strict)
 	inputFile: input file, one JSON per line
 	outputFile: output file
+        outputParquetFile: output parquet sink
 
 Example to explore output in spark-shell:
 $ spark-shell --jars target/scala-2.10/BillAnalysis-assembly-1.0.jar 
@@ -31,120 +31,39 @@ import org.apache.spark.sql.types._
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.WrappedArray
 
-import org.apache.spark.ml.feature.{HashingTF, IDF, RegexTokenizer, Tokenizer, NGram, StopWordsRemover}
-import org.apache.spark.ml.clustering.{KMeans, BisectingKMeans}
+//import org.apache.spark.ml.feature.{HashingTF, IDF, RegexTokenizer, Tokenizer, NGram, StopWordsRemover}
+//import org.apache.spark.ml.clustering.{KMeans, BisectingKMeans}
+//import org.apache.spark.mllib.clustering.KMeans
 
 import org.apache.spark.mllib.linalg.{Matrix, Matrices}
-import org.apache.spark.mllib.linalg.SingularValueDecomposition
+//import org.apache.spark.mllib.linalg.SingularValueDecomposition
 import org.apache.spark.mllib.linalg.distributed.RowMatrix
 
 //we have to deal with this nonsense for now
-import org.apache.spark.mllib.linalg.{
-  Vector => OldVector, Vectors => OldVectors, VectorUDT => OldVectorUDT}
+//import org.apache.spark.mllib.linalg.{
+//  Vector => OldVector, 
+//  Vectors => OldVectors, 
+//  SparseVector => OldSparseVector,
+//  DenseVector => OldDenseVector,
+//  VectorUDT => OldVectorUDT}
 
-import org.apache.spark.ml.linalg.{
-   Vector => NewVector,
-   DenseVector => NewDenseVector,
-   SparseVector => NewSparseVector
-}
+import org.apache.spark.ml.linalg.SQLDataTypes.VectorType
+
+//import org.apache.spark.ml.linalg.{
+//   Vector => NewVector,
+//   Vectors => NewVectors,
+//   DenseVector => NewDenseVector,
+//   SparseVector => NewSparseVector
+//}
 
 import java.io._
-import org.apache.spark.sql.functions.monotonicallyIncreasingId
+//import org.apache.spark.sql.functions.monotonicallyIncreasingId
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.DataFrame
+import org.scalatest.Assertions._
+
 
 object MakeLabeledCartesian {
-
-  //FIXME think about more genmeral way to convert dataframes to RDD and back
-  def converted2(row: scala.collection.Seq[Any]) : NewVector = {
-    val ret = row.asInstanceOf[WrappedArray[Any]]
-    ret(0).asInstanceOf[NewVector]
-  }
-
-  /**
-   * Finds the product of a distributed matrix and a diagonal matrix represented by a vector.
-   */
-  def multiplyByDiagonalMatrix(mat: RowMatrix, diag: OldVector): RowMatrix = {
-    val sArr = diag.toArray
-    new RowMatrix(mat.rows.map(vec => {
-      val vecArr = vec.toArray
-      val newArr = (0 until vec.size).toArray.map(i => vecArr(i) * sArr(i))
-      OldVectors.dense(newArr)
-    }))
-  }
-
-  def toOld(v: NewVector): OldVector = v match {
-    case sv: NewSparseVector => OldVectors.sparse(sv.size, sv.indices, sv.values)
-    case dv: NewDenseVector => OldVectors.dense(dv.values)
-  }
-
-  def pairup (document: MetaLabeledDocument, thewholething: org.apache.spark.broadcast.Broadcast[Array[MetaLabeledDocument]], strict_params: Tuple4[Boolean, Int, java.lang.String, Int], onlyInOut: Boolean) : (MetaLabeledDocument, Array[CartesianPair]) = {
-
-    val documents = thewholething.value
-
-    val (use_strict,strict_state,strict_docid,strict_year) = strict_params
-
-    val idocversion = document.docversion
-    val istate = document.state
-    val iyear = document.year
-    val idocid = document.docid
-    val pk1 = document.primary_key
-    val label1 = document.prediction
-
-    //var output_premap: Tuple2(MetaLabeledDocument, Array[CartesianPair]) = Tuple2()
-    var output_arr: ArrayBuffer[CartesianPair] = new ArrayBuffer[CartesianPair]()
-
-    for (jevent <- documents) {
-       val jdocversion = jevent.docversion
-       val jstate = jevent.state
-       val jyear = jevent.year
-       val pk2 = jevent.primary_key
-       val label2 = jevent.prediction
-       if (use_strict) {
-         //extra condition
-         if (istate == strict_state && idocid == strict_docid && iyear == strict_year) {
-           if (pk1 < pk2 && label1 == label2 && istate != jstate) {
-              var output: CartesianPair = CartesianPair(pk1,pk2)
-              output_arr += output
-           }
-         } 
-       } else {
-          //simple condition
-          if (onlyInOut) {
-             if (pk1 < pk2 && label1 == label2 && istate != jstate) {
-                var output: CartesianPair = CartesianPair(pk1,pk2)
-                output_arr += output
-             }
-           } else {
-             //in-out and in-in
-             if (pk1 < pk2 && label1 == label2) {
-                var output: CartesianPair = CartesianPair(pk1,pk2)
-                output_arr += output
-             }
-           } 
-        }
-     }
-     (document,output_arr.toArray)
-  }
-
-  //get type of var utility 
-  def manOf[T: Manifest](t: T): Manifest[T] = manifest[T]
-
-  //Calculate submatrix containing nColsToKeep of inital matrix A. This is used for truncating the V.T matrix in SVD 
-  def truncatedMatrix(a: Matrix, nColsToKeep: Int): Matrix = {
-      Matrices.dense(a.numRows,nColsToKeep,a.toArray.slice(0,a.numRows*nColsToKeep))
-  }
-
-  def customNPartitions(directory: File) : Int = {
-      var len = 0.0
-      val all: Array[File] = directory.listFiles()
-      for (f <- all) {
-        if (f.isFile())
-            len = len + f.length()
-        else
-            len = len + customNPartitions(f)
-      }
-      //353 GB worked with 7000 partitions
-      (7*len/350000000).toInt
-  }
 
   def main(args: Array[String]) {
 
@@ -160,6 +79,19 @@ object MakeLabeledCartesian {
     println("Elapsed time: " + (t1 - t0)/1000000000 + "s")
   }
 
+  def compactSelector_udf = udf((s: String) => {
+
+       val probe = s.toLowerCase()
+
+       val compactPattern = "compact".r
+       val isCompact = compactPattern.findFirstIn(probe).getOrElse("")
+
+       val uniformPattern = "uniform".r
+       val isUniform = uniformPattern.findFirstIn(probe).getOrElse("")
+
+       (isCompact.isEmpty() && isUniform.isEmpty())
+    })
+
   def run(params: Config) {
 
     val spark = SparkSession.builder().appName("MakeLabeledCartesian")
@@ -172,114 +104,72 @@ object MakeLabeledCartesian {
 
     import spark.implicits._
 
-    def compactSelector_udf = udf((s: String) => {
-
-         val probe = s.toLowerCase()
-
-         val compactPattern = "compact".r
-         val isCompact = compactPattern.findFirstIn(probe).getOrElse("")
-
-         val uniformPattern = "uniform".r
-         val isUniform = uniformPattern.findFirstIn(probe).getOrElse("")
-
-         (isCompact.isEmpty() && isUniform.isEmpty())
-      })
-
     val vv: String = params.getString("makeCartesian.docVersion") //like "Enacted"
     val input = spark.read.json(params.getString("makeCartesian.inputFile")).filter($"docversion" === vv).filter(compactSelector_udf(col("content")))
     input.printSchema()
     input.show()
 
     val npartitions = (4*input.count()/1000).toInt
-
     val bills = input.repartition(Math.max(npartitions,200),col("primary_key"),col("content"))
     bills.explain
 
-    //sqlContext.udf.register("cleaner_udf", (s: String) => s.replaceAll("(\\d|,|:|;|\\?|!)", ""))
-    def cleaner_udf = udf((s: String) => s.replaceAll("(\\d|,|:|;|\\?|!)", ""))
-    val cleaned_df = bills.withColumn("cleaned",cleaner_udf(col("content")))  //.drop("content")
+    val nGramGranularity = params.getInt("makeCartesian.nGramGranularity")
+    val addNGramFeatures = params.getBoolean("makeCartesian.addNGramFeatures")
+    val numTextFeatures = params.getInt("makeCartesian.numTextFeatures")
+    val useLSA = params.getBoolean("makeCartesian.useLSA")
+    val kval = params.getInt("makeCartesian.kval")
+    var rescaled_df = Utils.extractFeatures(bills,numTextFeatures,addNGramFeatures,nGramGranularity)
+    rescaled_df = rescaled_df.cache()
 
-    //tokenizer = Tokenizer(inputCol="text", outputCol="words")
-    var tokenizer = new RegexTokenizer().setInputCol("cleaned").setOutputCol("words").setPattern("\\W")
-    val tokenized_df = tokenizer.transform(cleaned_df)
+    val clusters_schema = StructType(Seq(StructField("primary_key",StringType,false),StructField("docversion",StringType, false),StructField("docid",StringType,false),StructField("state",LongType,false),StructField("year",LongType,false),StructField("features", VectorType, false),StructField("prediction",LongType,false)))
+    var clusters_df: DataFrame = spark.createDataFrame(spark.sparkContext.emptyRDD[Row], clusters_schema)
 
-    //remove stopwords 
-    var remover = new StopWordsRemover().setInputCol("words").setOutputCol("filtered")
-    var prefeaturized_df = remover.transform(tokenized_df).drop("words")
+    if (useLSA) {
+        //Apply low-rank matrix factorization via SVD approach, truncate to reduce the dimensionality
+        val dataPart2 = rescaled_df.select("primary_key","docversion","docid","state","year").rdd.map(row => Utils.converted3(row.toSeq)).zipWithIndex().map(x => (x._1._1,x._1._2,x._1._3,x._1._4,x._1._5,x._2)).toDF("primary_key","docversion","docid","state","year","id")
+        //val dataPart2 = rescaled_df.select("primary_key","docversion","docid","state","year").withColumn("id",monotonically_increasing_id)
 
-    //hashing
-    var hashingTF = new HashingTF().setInputCol("filtered").setOutputCol("rawFeatures").setNumFeatures(params.getInt("makeCartesian.numTextFeatures"))
-    val featurized_df = hashingTF.transform(prefeaturized_df)
+        val dataRDD = rescaled_df.select("features").rdd.map(row => Utils.converted2(row.toSeq)).map(x => Utils.toOld(x)).cache()
 
-    var idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
-    //val Array(train, cv) = featurized_df.randomSplit(Array(0.7, 0.3))
-    var idfModel = idf.fit(featurized_df)
-    val rescaled_df = idfModel.transform(featurized_df).drop("rawFeatures")
+        // Compute the top 5 singular values and corresponding singular vectors.
+        //320 concepts worked perfectly for 10 states
+        val numConcepts = params.getInt("makeCartesian.numConcepts")
+        rescaled_df = Utils.LSA(spark,dataRDD,numConcepts,2*numConcepts)
+        rescaled_df.show()
+        rescaled_df.printSchema
+        //assert(dataPart2.count() == rescaled_df.count())
 
-    //Apply low-rank matrix factorization via SVD approach, truncate to reduce the dimensionality
-    val dataPart2 = rescaled_df.select("primary_key","docversion","docid","state","year","content").withColumn("index",monotonically_increasing_id).cache()
+        clusters_df = Utils.KMeansSuite(rescaled_df,kval)
 
-    val dataRDD = rescaled_df.select("features").rdd.map(row => converted2(row.toSeq)).map(x => toOld(x)).cache()
-    val mat: RowMatrix = new RowMatrix(dataRDD) //that assumes RDD of Vectors
+        //Setup for splitting by cluster on the step2
+        val dataPart1 = clusters_df.select("prediction","features").rdd.map(row => Utils.converted4(row.toSeq)).zipWithIndex().map(x => (x._1._1,x._1._2,x._2)).toDF("prediction","features","id")
+        //val dataPart1 = clusters_df.select("prediction").withColumn("id",monotonicallyIncreasingId)
 
-    // Compute the top 5 singular values and corresponding singular vectors.
-    val numConcepts = 5
-    val svd: SingularValueDecomposition[RowMatrix, Matrix] = mat.computeSVD(numConcepts, computeU = true)
-    val U: RowMatrix = svd.U  // The U factor is a RowMatrix.
-    val s: OldVector = svd.s  // The singular values are stored in a local dense vector.
-    val VT: Matrix = svd.V.transpose  // The V factor is a local dense matrix.
-    //println("Singular values: " + svd.s)
-    //println(V.numRows,V.numCols)
-    var us: RowMatrix = multiplyByDiagonalMatrix(U,s)
-    var reconstructed = us.multiply(truncatedMatrix(VT,numConcepts)).rows.map(x => Row(x))
-    val reco_schema = StructType(List(StructField("features", new OldVectorUDT(), true)))
-    val reco_df = spark.createDataFrame(reconstructed,reco_schema)
-    reco_df.show()
-    reco_df.printSchema
-    println(reco_df.count())
-    println(dataPart2.count())
+        clusters_df = dataPart1.join(dataPart2, dataPart2("id") === dataPart1("id"))
+        //assert(dataPart1.count() == clusters_df.count())
 
-    // Trains a k-means model
-    // setDefault(
-    //k -> 2,
-    //maxIter -> 20,
-    //initMode -> MLlibKMeans.K_MEANS_PARALLEL,
-    //initSteps -> 5,
-    //tol -> 1e-4)
-    val kmeans = new KMeans().setK(params.getInt("makeCartesian.kval")).setMaxIter(40).setFeaturesCol("features").setPredictionCol("prediction")
-    //val kmeans = new BisectingKMeans().setK(params.getInt("makeCartesian.kval")).setSeed(1).setMaxIter(40).setFeaturesCol("features").setPredictionCol("prediction")
-    val model = kmeans.fit(rescaled_df)
-    var clusters_df = model.transform(rescaled_df)
+        clusters_df.printSchema()
+        clusters_df.show()
+        dataPart1.unpersist()
+        dataPart2.unpersist()
+    } else {
+        clusters_df = Utils.KMeansSuite(rescaled_df,kval)
+    }
+ 
+    clusters_df.select("primary_key","docversion","docid","state","year","prediction","features").write.parquet(params.getString("makeCartesian.outputParquetFile"))
 
-    val WSSSE = model.computeCost(rescaled_df)
-    println("Within Set Sum of Squared Errors = " + WSSSE)
-    model.explainParams()
-    val explained = model.extractParamMap()
-    println(explained)
-
-    //Setup for splitting by cluster on the step2
-    val dataPart1 = clusters_df.select("prediction").withColumn("index",monotonicallyIncreasingId).cache()
-    clusters_df = dataPart1.join(dataPart2, dataPart2("index") === dataPart1("index"))
-    println(dataPart1.count())
-    println(clusters_df.count())
-    clusters_df.printSchema()
-    clusters_df.show()
-
-    //clusters_df.select("primary_key","docversion","docid","state","year","prediction","content").write.parquet(params.getString("makeCartesian.outputParquetFile"))
-
-    var bills_meta = clusters_df.select("primary_key","docversion","docid","state","year","prediction").as[MetaLabeledDocument]
+    var bills_meta = clusters_df.select("primary_key","docversion","docid","state","year","prediction").as[MetaLabeledDocument].cache()
     var bills_meta_bcast = spark.sparkContext.broadcast(bills_meta.collect())
 
     val strict_params = (params.getBoolean("makeCartesian.use_strict"),params.getInt("makeCartesian.strict_state"),params.getString("makeCartesian.strict_docid"),params.getInt("makeCartesian.strict_year"))
 
     //will be array of tuples, but the keys are unique
     var cartesian_pairs = bills_meta.rdd.coalesce(params.getInt("makeCartesian.nPartitions"))
-                          .map(x => pairup(x,bills_meta_bcast, strict_params, params.getBoolean("makeCartesian.onlyInOut")))
+                          .map(x => Utils.pairup(x,bills_meta_bcast, strict_params, params.getBoolean("makeCartesian.onlyInOut")))
                           .filter({case (dd,ll) => (ll.length > 0)})
                           .map({case(k,v) => v}).flatMap(x => x) //.groupByKey()    
 
     cartesian_pairs.saveAsObjectFile(params.getString("makeCartesian.outputFile"))
-
     spark.stop()
    }
 }
