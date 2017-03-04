f088c3e57d62e6d059c805e02fd1bc7dac7c8b7b
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-03-03 22:30:01 -0500

    Improve stability of join for larger datasets (see inline comments for details)

--- src/main/scala/org/princeton/billmatch/stats/AnalysisUtils.scala
+++ src/main/scala/org/princeton/billmatch/stats/AnalysisUtils.scala
@@ -39,14 +39,16 @@ object AnalysisUtils {
   def sampleNOrdered(spark: SparkSession, raw_input_filename: String, processed_input_filename: String, numRows: Int, isAscending: Boolean) : DataFrame = {  
     import spark.implicits._
 
-    var raw_bills = spark.read.json(raw_input_filename).filter(col("docversion") === "Introduced")
-    raw_bills = raw_bills.withColumn("content",Utils.cleaner_udf(col("content")))
-    val processed_data = spark.read.parquet(processed_input_filename).cache()
-    var sorted = processed_data.sort(desc("similarity")).limit(numRows)
-    if (isAscending) {
-       sorted = processed_data.sort(asc("similarity")).limit(numRows)
-    }
-    val j1 = sorted.join(raw_bills.withColumnRenamed("content","content1"),$"pk1" === $"primary_key").select("pk1","pk2","content1","similarity")
+    val input = spark.read.json(raw_input_filename).filter(col("docversion") === "Introduced")
+    val npartitions = (input.count()/1000).toInt
+
+    val raw_bills = input.withColumn("content",Utils.cleaner_udf(col("content"))).cache()
+
+    val processed_data = spark.read.parquet(processed_input_filename)
+    val sorted = if (isAscending) processed_data.sort(asc("similarity")).limit(numRows) else processed_data.sort(desc("similarity")).limit(numRows)
+    val sorted_dataset = sorted.as[ComparedPair].repartition(npartitions).cache()
+
+    val j1 = sorted_dataset.join(raw_bills.withColumnRenamed("content","content1"),$"pk1" === $"primary_key").select("pk1","pk2","content1","similarity")
     val j2 = j1.join(raw_bills.withColumnRenamed("content","content2"),$"pk2" === $"primary_key").select("pk1","pk2","content1","content2","similarity") //.write.parquet("/user/path/to/output")
     //to sort by similarity do
     if (isAscending) {
@@ -59,15 +61,16 @@ object AnalysisUtils {
   def sampleNRandom(spark: SparkSession, raw_input_filename: String, processed_input_filename: String, numRows: Int, isAscending: Boolean, threshold: Double) : DataFrame = {
     import spark.implicits._
 
-    var raw_bills = spark.read.json(raw_input_filename).filter(col("docversion") === "Introduced")
-    raw_bills = raw_bills.withColumn("content",Utils.cleaner_udf(col("content")))
-    val processed_data = spark.read.parquet(processed_input_filename).cache()
-    var sorted = processed_data.filter(processed_data("similarity") >= threshold)
-    if (isAscending) {
-       sorted = processed_data.filter(processed_data("similarity") <= threshold)
-    }
+    val input = spark.read.json(raw_input_filename).filter(col("docversion") === "Introduced")
+    val npartitions = (input.count()/1000).toInt
+
+    val raw_bills = input.withColumn("content",Utils.cleaner_udf(col("content"))).cache()
+
+    val processed_data = spark.read.parquet(processed_input_filename)
+    val sorted = if (isAscending) processed_data.sort(asc("similarity")).limit(numRows) else processed_data.sort(desc("similarity")).limit(numRows)
+    val sorted_dataset = sorted.as[ComparedPair].repartition(npartitions).cache()
     
-    val j1 = sorted.join(raw_bills.withColumnRenamed("content","content1"),$"pk1" === $"primary_key").select("pk1","pk2","content1","similarity")
+    val j1 = sorted_dataset.join(raw_bills.withColumnRenamed("content","content1"),$"pk1" === $"primary_key").select("pk1","pk2","content1","similarity")
     val j2 = j1.join(raw_bills.withColumnRenamed("content","content2"),$"pk2" === $"primary_key").select("pk1","pk2","content1","content2","similarity")
 
     var fraction: Double = numRows.toDouble/sorted.count()
@@ -119,3 +122,4 @@ object AnalysisUtils {
   }
 
 }
+case class ComparedPair(pk1: String, pk2: String, similarity: Double)
