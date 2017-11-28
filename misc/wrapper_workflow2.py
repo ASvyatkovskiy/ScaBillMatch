a2436986c5c1b30f8f5899158f086ccbb36600a3
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-11-28 14:12:41 -0500

    Cleanup

--- misc/wrapper_workflow2.py
+++ misc/wrapper_workflow2.py
@@ -2,10 +2,10 @@
 
 from subprocess import Popen
 
-#Popen("sbt assembly",shell=True).wait()
-#Popen("spark-submit --class org.apache.spark.ml.feature.ExtractMinHashLSH --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
+Popen("sbt assembly",shell=True).wait()
+Popen("spark-submit --class org.apache.spark.ml.feature.ExtractMinHashLSH --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
 
-#Popen("spark-submit --class org.princeton.billmatch.utils.HarvestOutput --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
+Popen("spark-submit --class org.princeton.billmatch.utils.HarvestOutput --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
 
 config = open("src/main/resources/workflow2.conf","r").readlines()
 for line in config:
