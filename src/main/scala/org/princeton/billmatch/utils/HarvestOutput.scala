7d3585c5db76c3544f2766b89f0df7ecc11940d6
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-11-28 14:17:25 -0500

    Add harvesting step for workflow1

--- src/main/scala/org/princeton/billmatch/utils/HarvestOutput.scala
+++ src/main/scala/org/princeton/billmatch/utils/HarvestOutput.scala
@@ -18,10 +18,16 @@ object HarvestOutput {
   val params = ConfigFactory.load("harvester")
   val specific_class = params.getString("harvester.app")
   val specific_params = ConfigFactory.load(specific_class)
-  val path = specific_params.getString(specific_class+".outputFileBase")
+  if (specific_class == "workflow2") {
+     val path = specific_params.getString(specific_class+".outputFileBase")
 
-  // Loads data.
-  val input = spark.read.parquet(path+"*_*")
-  input.write.parquet(path) 
+     // Loads data.
+     val input = spark.read.parquet(path+"*_*")
+     input.write.parquet(path) 
+  } else {
+    val odata = spark.sparkContext.objectFile[Tuple2[Tuple2[String,String],Double]](specific_class+"_billAnalyzer.outputMainFile")
+    val odata_df = skimmed_data.map(x=>(x._1._1,x._1._2,x._2)).toDF("pk1","pk2","similarity")
+    odata_df.write.parquet(specific_class+"_billAnalyzer.outputMainFile"+"_df")
+  }
  }
 }
