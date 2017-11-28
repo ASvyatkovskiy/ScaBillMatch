c7497b202c9cf2fdf5ff37e96c0cb136abc35ee3
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-11-28 14:17:37 -0500

    Add harvesting step for workflow1

--- misc/wrapper_workflow1.py
+++ misc/wrapper_workflow1.py
@@ -12,3 +12,16 @@ for comb in range(14):
     Popen("cp billAnalyzer"+str(comb)+".conf src/main/resources/billAnalyzer.conf",shell=True).wait()
     Popen("sbt assembly",shell=True).wait()
     Popen("spark-submit --class org.princeton.billmatch.BillAnalyzer --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 15g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
+
+
+
+#run harvester changing app=workflow1
+Popen("spark-submit --class org.princeton.billmatch.utils.HarvestOutput --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
+
+#cleanup folders
+config = open("src/main/resources/workflow1_billAnalyzer.conf","r").readlines()
+for line in config:
+    if "outputMainFile" in line: outputMainFile = line.split("=")[-1].lstrip(" \"").rstrip("\",\n")
+outputMainFile = "_".join(outputMainFile.split("_")[:-1])+"_*"
+Popen("hdfs dfs -rmr "+outputMainFile,shell=True).wait()
+
