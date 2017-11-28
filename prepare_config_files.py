1438bb5af3b04dc7a941882386eb2f41e5394e37
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-11-28 14:36:54 -0500

    Refactor

---
+++ prepare_config_files.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import sys
+
+def createConfigsA(configBaseName, nPartitions, nCPartitions, measureName, inputParquetFile, inputPairsFile, outputMainFile, nFolders):
+    def createOneConfig(nFolders):
+        if nFolders < 0: return
+        with open(configBaseName+str(nFolders)+".conf","w") as f:
+            f.write("billAnalyzer {\n")
+            f.write("  nPartitions  = "+str(nPartitions)+",\n")
+            f.write("  nCPartitions = "+str(nCPartitions)+",\n")
+            f.write("  measureName = \""+measureName+"\",\n")
+            f.write("  inputParquetFile = \""+inputParquetFile+"\",\n")
+            f.write("  inputPairsFile = \""+inputPairsFile+"/p"+str(nFolders)+"\",\n")
+            f.write("  outputMainFile = \""+outputMainFile+"_"+str(nFolders)+"\",\n")
+            f.write("}\n")
+        createOneConfig(nFolders-1)
+    createOneConfig(nFolders)
+
+def createConfigsB(nPartitions, nCPartitions, measureName, inputParquetFile, inputPairsFile, outputMainFile, strictCats):
+    def createOneConfig(cat):
+        configBaseName = "makeCartesian" 
+        with open(configBaseName+cat+".conf","w") as f:
+            f.write(configBaseName+" {\n")
+            f.write("    cat = \""+cat+"\",\n")
+            f.write("    optimizationLevel = 3,\n")
+            f.write("    docVersion   = \"Introduced\",\n")
+            f.write("    useLSA       = false,\n")
+            f.write("    onlyInOut    = true,\n")
+            f.write("    nGramGranularity = 5,\n")
+            f.write("    addNGramFeatures = true,\n")
+            f.write("    inputFile    = \"file:///scratch/network/alexeys/bills/lexs/bills_combined_50_p*.json\",\n")
+            f.write("    outputFile   = \"/user/alexeys/valid_pairs_\""+cat+",\n")
+            f.write("    outputParquetFile = \"/user/alexeys/bills_combined_\""+cat+",\n")
+            f.write("    use_strict   = false,\n")
+            f.write("    strict_state = 8,\n")
+            f.write("    strict_docid = \"SB436\",\n")
+            f.write("    strict_year  = 2005,\n")
+            f.write("    nPartitions  = 40,\n")
+            f.write("    numTextFeatures = 1048576,\n")
+            f.write("    kval = 150,\n")
+            f.write("    numConcepts = 1500\n")
+            f.write("}\n")
+    for cat in strictCats:
+        createOneConfig(cat)
+
+
+if __name__=='__main__':
+    nFolders = 11 #14
+    nPartitions = 120
+    nCPartitions = 3000
+    measureName = "maxasymjaccard"
+    inputParquetFile = sys.argv[1] #"/user/alexeys/bills_combined50_300k_replica2_unigram"
+    inputPairsFile = sys.argv[2] #"/user/alexeys/valid_pairs_50_300k_replica2_unigram"
+    outputMainFile = sys.argv[3] #"/user/alexeys/output50_300k_replica2_unigram"
+    configBaseName = sys.argv[4]
+    createConfigsA(configBaseName, nPartitions, nCPartitions, measureName, inputParquetFile, inputPairsFile, outputMainFile, nFolders)
---
+++ wrapper_workflow2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from subprocess import Popen
+
+Popen("sbt assembly",shell=True).wait()
+Popen("spark-submit --class org.apache.spark.ml.feature.ExtractMinHashLSH --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
+
+Popen("spark-submit --class org.princeton.billmatch.utils.HarvestOutput --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
+
+config = open("src/main/resources/workflow2.conf","r").readlines()
+for line in config:
+    if "outputFileBase" in line: outputFileBase = line.split("=")[-1].lstrip(" \"").rstrip("\",\n")+"*_*"
+
+Popen("hdfs dfs -rmr "+outputFileBase,shell=True).wait()
