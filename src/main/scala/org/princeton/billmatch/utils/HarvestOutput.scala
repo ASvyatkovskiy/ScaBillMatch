186389dd1c4b78937d31937d3e31dd5031afed50
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-11-28 22:21:36 -0500

    Working version

--- src/main/scala/org/princeton/billmatch/utils/HarvestOutput.scala
+++ src/main/scala/org/princeton/billmatch/utils/HarvestOutput.scala
@@ -17,17 +17,22 @@ object HarvestOutput {
 
   val params = ConfigFactory.load("harvester")
   val specific_class = params.getString("harvester.app")
-  val specific_params = ConfigFactory.load(specific_class)
   if (specific_class == "workflow2") {
+     val specific_params = ConfigFactory.load(specific_class)
      val path = specific_params.getString(specific_class+".outputFileBase")
 
      // Loads data.
      val input = spark.read.parquet(path+"*_*")
      input.write.parquet(path) 
   } else {
-    val odata = spark.sparkContext.objectFile[Tuple2[Tuple2[String,String],Double]](specific_class+"_billAnalyzer.outputMainFile")
+    val specific_params = ConfigFactory.load(specific_class+"_billAnalyzer")
+    var path = specific_params.getString(specific_class+"_billAnalyzer.outputMainFile")
+    val p = path.split("_").dropRight(1)
+    path = p.mkString("_")
+
+    val odata = spark.sparkContext.objectFile[Tuple2[Tuple2[String,String],Float]](path+"_*")
     val odata_df = odata.map(x=>(x._1._1,x._1._2,x._2)).toDF("pk1","pk2","similarity")
-    odata_df.write.parquet(specific_class+"_billAnalyzer.outputMainFile"+"_df")
+    odata_df.write.parquet(path)
   }
  }
 }
