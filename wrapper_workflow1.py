e933b756efeb877529bd25b3788e6ba5a25dac1f
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-12-01 11:57:18 -0500

    Use getpass.getuser, introduce arguments for HDFS path and Jar paths

--- wrapper_workflow1.py
+++ wrapper_workflow1.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python
 
+import sys,os,getpass
 from subprocess import Popen
 
-Popen("sbt assembly",shell=True).wait()
-Popen("spark-submit --class org.princeton.billmatch.ExtractCandidates --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
 
-Popen("python prepare_valid_pairs.py /user/alexeys/valid_pairs/",shell=True).wait()
-Popen("python prepare_config_files.py /user/alexeys/bills_combined /user/alexeys/valid_pairs /user/alexeys/output_sample",shell=True).wait()
+base_hdfs_path ="/user"
+jar_path = "target/scala-2.11/BillAnalysis-assembly-2.0.jar"
+
+Popen("sbt assembly",shell=True).wait()
+Popen("spark-submit --class org.princeton.billmatch.ExtractCandidates --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g {}".format(jar_path),shell=True).wait()
+
+Popen("python prepare_valid_pairs.py {}".format(os.path.join(base_hdfs_path,getpass.getuser(),"valid_pairs")),shell=True).wait()
+Popen("python prepare_config_files.py {} {} {}".format(os.path.join(base_hdfs_path,getpass.getuser(),"bills_combined"), os.path.join(base_hdfs_path,getpass.getuser(),"valid_pairs"),os.path.join(base_hdfs_path,getpass.getuser(),"output_sample"))
 
 for comb in range(14):
     Popen("cp workflow1_billAnalyzer"+str(comb)+".conf src/main/resources/workflow1_billAnalyzer.conf",shell=True).wait()
     Popen("sbt assembly",shell=True).wait()
-    Popen("spark-submit --class org.princeton.billmatch.BillAnalyzer --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 15g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
+    Popen("spark-submit --class org.princeton.billmatch.BillAnalyzer --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 15g --driver-memory 20g {}".format(jar_path),shell=True).wait()
 
 #run harvester changing app=workflow1
-Popen("spark-submit --class org.princeton.billmatch.utils.HarvestOutput --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
+Popen("spark-submit --class org.princeton.billmatch.utils.HarvestOutput --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g {}".format(jar_path),shell=True).wait()
 
 #cleanup folders
 config = open("src/main/resources/workflow1_billAnalyzer.conf","r").readlines()
--- wrapper_workflow2.py
+++ wrapper_workflow2.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
+import sys,os,getpass
 from subprocess import Popen
 
-Popen("sbt assembly",shell=True).wait()
-Popen("spark-submit --class org.apache.spark.ml.feature.ExtractMinHashLSH --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
+jar_path = "target/scala-2.11/BillAnalysis-assembly-2.0.jar"
 
-Popen("spark-submit --class org.princeton.billmatch.utils.HarvestOutput --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g target/scala-2.11/BillAnalysis-assembly-2.0.jar",shell=True).wait()
+Popen("sbt assembly",shell=True).wait()
+Popen("spark-submit --class org.apache.spark.ml.feature.ExtractMinHashLSH --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g {}".format(jar_path),shell=True).wait()
+
+Popen("spark-submit --class org.princeton.billmatch.utils.HarvestOutput --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g {}".format(jar_path),shell=True).wait()
 
 config = open("src/main/resources/workflow2.conf","r").readlines()
 for line in config:
