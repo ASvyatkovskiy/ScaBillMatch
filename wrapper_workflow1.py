e79cf45019f11cc1bbfb7d9c334601a9d0e86f1b
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-12-05 09:31:49 -0500

    Add postptocessing step to the workflow1

--- wrapper_workflow1.py
+++ wrapper_workflow1.py
@@ -3,7 +3,6 @@
 import sys,os,getpass
 from subprocess import Popen
 
-
 base_hdfs_path ="/user"
 jar_path = "target/scala-2.11/BillAnalysis-assembly-2.0.jar"
 
@@ -11,7 +10,7 @@ Popen("sbt assembly",shell=True).wait()
 Popen("spark-submit --class org.princeton.billmatch.ExtractCandidates --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g {}".format(jar_path),shell=True).wait()
 
 Popen("python prepare_valid_pairs.py {}".format(os.path.join(base_hdfs_path,getpass.getuser(),"valid_pairs")),shell=True).wait()
-Popen("python prepare_config_files.py {} {} {}".format(os.path.join(base_hdfs_path,getpass.getuser(),"bills_combined"), os.path.join(base_hdfs_path,getpass.getuser(),"valid_pairs"),os.path.join(base_hdfs_path,getpass.getuser(),"output_sample"))
+Popen("python prepare_config_files.py {} {} {}".format(os.path.join(base_hdfs_path,getpass.getuser(),"bills_combined"), os.path.join(base_hdfs_path,getpass.getuser(),"valid_pairs"),os.path.join(base_hdfs_path,getpass.getuser(),"output_sample")),shell=True).wait()
 
 for comb in range(14):
     Popen("cp workflow1_billAnalyzer"+str(comb)+".conf src/main/resources/workflow1_billAnalyzer.conf",shell=True).wait()
@@ -27,4 +26,9 @@ for line in config:
     if "outputMainFile" in line: outputMainFile = line.split("=")[-1].lstrip(" \"").rstrip("\",\n")
 outputMainFile = "_".join(outputMainFile.split("_")[:-1])
 Popen("hdfs dfs -rmr "+outputMainFile+"_*",shell=True).wait()
-print("The workflow1 results are in folder {}".format(outputMainFile)
+print("The workflow1 results are in folder {}".format(outputMainFile))
+
+#do postprocessing
+Popen("spark-submit --class org.princeton.billmatch.utils.Postprocessor --master yarn --deploy-mode client --queue production --num-executors 40 --executor-cores 3 --executor-memory 16g --driver-memory 20g {}".format(jar_path),shell=True).wait()
+#print("The workflow1 skim results are in folder {}".format(outputMainFile))
+#print("The workflow1 light results are in folder {}".format(outputMainFile))
