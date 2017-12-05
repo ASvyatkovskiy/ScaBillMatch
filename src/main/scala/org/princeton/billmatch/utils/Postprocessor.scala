37546de7be47d7aaf35a73c5f19c0659bca46965
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-12-05 09:29:57 -0500

    Add postprocessor class to save in skim and light formats. Works the same for each workflow

---
+++ src/main/scala/org/princeton/billmatch/utils/Postprocessor.scala
@@ -0,0 +1,47 @@
+package org.princeton.billmatch.utils
+
+import org.apache.spark.{SparkConf, SparkContext, SparkFiles}
+import org.apache.spark.sql.SparkSession
+
+import com.typesafe.config._
+import org.princeton.billmatch.stats._
+
+object Postprocessor { 
+
+ def main(args: Array[String]) {
+
+  val spark = SparkSession.builder().appName("Postprocessor")
+    .config("spark.driver.maxResultSize", "10g")
+    .getOrCreate()
+
+  import spark.implicits._
+
+  val params = ConfigFactory.load("postprocessor")
+  val inputJsonPath = params.getString("postprocessor.inputJsonPath")
+  val inputResultParquet = params.getString("postprocessor.inputResultParquet")
+  val getNBest = params.getInt("postprocessor.getNBest") 
+  val isAscending = params.getBoolean("postprocessor.isAscending")
+  //val makeLight = params.getBoolean("postprocessor.makeLight")
+  val outputSkimFile = params.getString("postprocessor.outputSkimFile")
+  val outputLightFile = params.getString("postprocessor.outputLightFile") 
+
+  //takes the output of the steps2 of the bill analysis
+  val skimmed_data = AnalysisUtils.sampleNOrdered(spark,inputJsonPath,inputResultParquet,getNBest,isAscending,false)
+
+  //imposes temporal order among key columns
+  val ordered_skim_data = AnalysisUtils.imposeTemporalOrder(skimmed_data)
+
+  //saves to the skimmed
+  ordered_skim_data.repartition(1).write.json(outputSkimFile)
+
+  //to get light specify:
+  val light_data = AnalysisUtils.sampleNOrdered(spark,inputJsonPath,inputResultParquet,-1,isAscending,true)
+
+  //imposes temporal order among key columns
+  val ordered_light_data = AnalysisUtils.imposeTemporalOrder(light_data)
+
+  //saves to the light
+  ordered_light_data.write.json(outputLightFile)
+
+ }
+}
