5ed939552fcd95a4b7559801ce941f7eb7ca2265
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-12-05 09:34:15 -0500

    Cleanup. Use ConfigFactory

--- prepare_config_files.py
+++ prepare_config_files.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import sys
+from pyhocon import ConfigFactory
 
-def createConfigsA(nPartitions, nCPartitions, measureName, inputParquetFile, inputPairsFile, outputMainFile, nFolders):
-    def createOneConfig(nFolders):
+def createConfigsA(conf,nPartitions, nCPartitions, measureName, inputParquetFile, inputPairsFile, outputMainFile, nFolders):
+    def createOneConfig(conf,nFolders):
+        #maybe assert for it
         configBaseName = "workflow1_billAnalyzer"
         if nFolders < 0: return
         with open(configBaseName+str(nFolders)+".conf","w") as f:
@@ -14,23 +16,35 @@ def createConfigsA(nPartitions, nCPartitions, measureName, inputParquetFile, inp
             f.write("  inputPairsFile = \""+inputPairsFile+"/p"+str(nFolders)+"\",\n")
             f.write("  outputMainFile = \""+outputMainFile+"_"+str(nFolders)+"\",\n")
             f.write("}\n")
-        createOneConfig(nFolders-1)
-    createOneConfig(nFolders)
+        createOneConfig(conf,nFolders-1)
+    createOneConfig(conf,nFolders)
 
-def createConfigsB(nPartitions, nCPartitions, measureName, inputParquetFile, inputPairsFile, outputMainFile, strictCats):
-    def createOneConfig(cat):
-        configBaseName = "workflow1_makeCartesian" 
+def createConfigsB(conf,nPartitions, nCPartitions, measureName, inputParquetFile, inputPairsFile, outputMainFile, strictCats):
+    def createOneConfig(conf):
+        configBaseName,conf_items = conf.items() #"workflow1_makeCartesian" 
+        with open(configBaseName+".conf","w") as f:
+            f.write(configBaseName+" {\n")
+            if cat != "":
+                f.write("    cat = \""+cat+"\",\n")
+            for k,v in conf_items: 
+                f.write("    {}={},\n".format(k,v))
+            f.write("}\n")
+    createOneConfig(conf)
+
+def createConfigsC(conf,cat,nPartitions, nCPartitions, measureName, inputParquetFile, inputPairsFile, outputMainFile, strictCats):
+    def createOneConfig(conf,cat):
+        configBaseName,conf_items = conf.items() #"workflow1_makeCartesian" 
         with open(configBaseName+cat+".conf","w") as f:
             f.write(configBaseName+" {\n")
             if cat != "":
                 f.write("    cat = \""+cat+"\",\n")
-            f.write("    optimizationLevel = 1,\n")
+            f.write("    optimizationLevel = 0,\n")
             f.write("    docVersion   = \"Introduced\",\n")
             f.write("    useLSA       = false,\n")
             f.write("    onlyInOut    = true,\n")
             f.write("    nGramGranularity = 5,\n")
             f.write("    addNGramFeatures = true,\n")
-            f.write("    inputFile    = \"file:///scratch/network/alexeys/bills/lexs/bills_combined_3_COILNJ_small.json\",\n")
+            f.write("    inputFile    = \"file:///scratch/network/alexeys/bills/lexs/bills_combined_3_COILNJ_new.json\",\n")
             if cat != "":
                 f.write("    outputFile   = \"/user/alexeys/valid_pairs_\""+cat+",\n")
                 f.write("    outputParquetFile = \"/user/alexeys/bills_combined_\""+cat+",\n")
@@ -43,15 +57,18 @@ def createConfigsB(nPartitions, nCPartitions, measureName, inputParquetFile, inp
             f.write("    numConcepts = 1500\n")
             f.write("}\n")
     for cat in strictCats:
-        createOneConfig(cat)
+        createOneConfig(conf,cat)
 
 
 if __name__=='__main__':
+    base_config_path = 'src/main/resources/workflow1_makeCartesian.conf'
+    conf = ConfigFactory.parse_file(base_config_path)
+
     nFolders = 14
     nPartitions = 120
     nCPartitions = 3000
     measureName = "maxasymjaccard"
-    inputParquetFile = sys.argv[1] #"/user/alexeys/bills_combined50_300k_replica2_unigram"
-    inputPairsFile = sys.argv[2] #"/user/alexeys/valid_pairs_50_300k_replica2_unigram"
-    outputMainFile = sys.argv[3] #"/user/alexeys/output50_300k_replica2_unigram"
-    createConfigsA(nPartitions, nCPartitions, measureName, inputParquetFile, inputPairsFile, outputMainFile, nFolders)
+    inputParquetFile = sys.argv[1] #/user/alexeys/bills_combined
+    inputPairsFile = sys.argv[2] #/user/alexeys/valid_pairs
+    outputMainFile = sys.argv[3] #/user/alexeys/output_sample
+    createConfigsA(conf, nPartitions, nCPartitions, measureName, inputParquetFile, inputPairsFile, outputMainFile, nFolders)
