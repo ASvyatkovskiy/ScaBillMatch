fe9f634563e531ff88e4b653a7e304fce9079d90
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-12-05 09:33:33 -0500

    Use os path join

--- prepare_valid_pairs.py
+++ prepare_valid_pairs.py
@@ -3,13 +3,13 @@
 from subprocess import Popen
 import glob
 from math import ceil
-import sys
+import sys,os
 
 BASE_VALID_DIR = sys.argv[1]
 FILES_PER_FOLDER = 3
 
 #Prepare sub-folder
-fileNames = glob.glob(BASE_VALID_DIR+"part*")
+fileNames = glob.glob(os.path.join(BASE_VALID_DIR,"part*"))
 totalFiles = len(fileNames)
 
 print "Found total {} files".format(totalFiles) 
@@ -18,7 +18,7 @@ nFolders = int(ceil((totalFiles+FILES_PER_FOLDER)/FILES_PER_FOLDER))
 
 print "Creating {} folders...".format(nFolders)
 for i in range(nFolders):
-    Popen("hdfs dfs -mkdir "+BASE_VALID_DIR+"p"+str(i),shell=True).wait()
+    Popen("hdfs dfs -mkdir "+os.path.join(BASE_VALID_DIR,"p"+str(i)),shell=True).wait()
 
 
 while fileNames:
@@ -26,6 +26,6 @@ while fileNames:
     for _ in range(FILES_PER_FOLDER):
         try: 
             n = fileNames.pop(0)
-            print "hdfs dfs -mv "+n+" "+BASE_VALID_DIR+"p"+str(nFolders)
-            Popen("hdfs dfs -mv "+n+" "+BASE_VALID_DIR+"p"+str(nFolders),shell=True).wait()
+            print "hdfs dfs -mv "+n+" "+os.path.join(BASE_VALID_DIR,"p"+str(nFolders))
+            Popen("hdfs dfs -mv "+n+" "+os.path.join(BASE_VALID_DIR,"p"+str(nFolders)),shell=True).wait()
         except IndexError: pass         
