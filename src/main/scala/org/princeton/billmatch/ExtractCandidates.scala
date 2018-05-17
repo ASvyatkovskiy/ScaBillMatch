36090dd82f9526463f545d254b2565a85ae8480a
Yuki Shiraito <shiraito@princeton.edu>
2018-05-17 17:32:41 -0400

    add switch between "bothIn" and "onlyOne" for baseline calculation

--- src/main/scala/org/princeton/billmatch/ExtractCandidates.scala
+++ src/main/scala/org/princeton/billmatch/ExtractCandidates.scala
@@ -152,6 +152,13 @@ object ExtractCandidates {
     if (predicate_path.length() > 0) { 
       val custom_predicate : String = Utils.makeCustomPredicate(predicate_path)        
       bills_meta_for_bcast = clusters_df.filter(custom_predicate).select("primary_key","docversion","docid","state","year","prediction","length").as[MetaLabeledDocument].collect()
+
+      if (params.getBoolean("bothIn")) {
+      	clusters_df = clusters_df.filter(custom_predicate)
+      } else {
+      	val custom_predicate_ex : String = Utils.makeCustomPredicateExclude(predicate_path)
+      	clusters_df = clusters_df.filter(custom_predicate_ex)
+      }
      } else {
       bills_meta_for_bcast = clusters_df.select("primary_key","docversion","docid","state","year","prediction","length").as[MetaLabeledDocument].collect()
     }
